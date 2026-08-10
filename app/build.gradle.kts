9b3a63a47e6d8e5520660bdcf7bb5ac364a2d4d3
baici <dimo9174@gmail.com>
2026-08-10 20:45:14 +0800

    Release 0.9.3-beta44

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "moe.lukoa.launcher"
         minSdk = 26
         targetSdk = 35
-        versionCode = 260
-        versionName = "0.9.3-beta43"
+        versionCode = 261
+        versionName = "0.9.3-beta44"
     }
 
     buildTypes {
